fb3c607848a4b487a9807c4921cd7c3cbe65ee8f
acey9 <huiwang.e@gmail.com>
2017-05-29 15:38:46 +0800

    reset

--- bot/worker.go
+++ bot/worker.go
@@ -72,9 +72,9 @@ func (w *Worker) StartBot(cmdStr string) {
 	}
 	w.startQueue <- b
 	bts.logger.Notice("Start <Bot:%s> %s", b.bid, b.cmdStr)
-	err := b.cmd.Process.Wait()
+	b.cmd.Process.Wait()
 	w.delBotsMapQueue <- b
-	bts.logger.Notice("Stoped <Bot:%s> %s info:%v", b.bid, b.cmdStr, err)
+	bts.logger.Notice("Stoped <Bot:%s> %s", b.bid, b.cmdStr)
 }
 
 func (w *Worker) CheckBot(cmdStr string) {
